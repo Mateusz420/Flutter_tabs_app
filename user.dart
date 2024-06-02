b0593e86b5a1d2fd96a77add099f3743745d5d90
Mateusz420 <145479028+Mateusz420@users.noreply.github.com>
2024-06-02 20:10:36 +0200

    Update user.dart
    Added the ID parameter

--- user.dart
+++ user.dart
@@ -1,11 +1,13 @@
 class User {
+  int? id;
   String username;
   String password;
 
-  User(this.username, this.password);
+  User({this.id, required this.username, required this.password});
 
   Map<String, dynamic> toMapUser() {
     return {
+      'id': id,
       'username': username,
       'password': password,
     };
